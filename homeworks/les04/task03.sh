9841707877d1daddb7d0ef621fb92b4eedd3b4b5
Alex Klyanchin <k142@mail.ru>
2020-11-26 00:23:30 +0300

    les 04 initial

---
+++ homeworks/les04/task03.sh
@@ -0,0 +1,7 @@
+# 3. Управление процессами.) Изменить конфигурационный файл службы SSH: /etc/ssh/sshd_config,
+# отключив аутентификацию по паролю PasswordAuthentication no.
+# Выполните рестарт службы systemctl restart sshd (service sshd restart),
+# верните аутентификацию по паролю, выполните reload службы systemctl reload sshd
+# (services sshd reload). В чём различие между действиями restart и reload?
+# Создайте файл при помощи команды cat > file_name, напишите текст и завершите комбинацией ctrl+d.
+# Какой сигнал передадим процессу?
